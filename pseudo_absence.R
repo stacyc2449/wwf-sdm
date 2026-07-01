eeb6833be7c87a035d1eba906a5c9bf1cb9159ec
Stacy Chen <Stacy.Chen-Intern@wwfus.org>
2026-07-01 14:24:04 -0400

    pseudo absence generation working, with distance of 200 km between presences

--- pseudo_absence.R
+++ pseudo_absence.R
@@ -4,6 +4,7 @@ library(dismo)
 library(sf)
 library(tigris)
 library(ggplot2)
+library(lwgeom)
 
 ncr_raw <- read.delim("northern_corn_rootworm_0003360-260623161305970\\ncr.csv", sep="\t")
 ncr_raw <- ncr_raw[which(ncr_raw$countryCode=="US"),]
@@ -11,7 +12,8 @@ ncr <- data.frame(matrix(ncol = 2, nrow = length(ncr_raw$decimalLongitude)))
 ncr[,1] <- ncr_raw$decimalLongitude
 ncr[,2] <- ncr_raw$decimalLatitude
 
-e <- ext(-125, -66.9, 24.5, 49.4) # set study area extent
+# study area extent for ncr presences
+e <- ext(-125, -66.9, 24.5, 49.4)
 
 ncr <- unique(ncr) # remove duplicates
 ncr <- ncr[complete.cases(ncr),] # remove na's
@@ -20,14 +22,34 @@ colnames(ncr) <- c('lon','lat')
 ncr <- ncr[which(ncr$lon>=e[1] & ncr$lon<=e[2]),]
 ncr <- ncr[which(ncr$lat>=e[3] & ncr$lat<=e[4]),]
 
+ncr_sf <- st_as_sf(ncr, coords = c("lon", "lat"), crs = 4326)
+
+#crs 102009 corresponds to us projection, in meters
+ncr_proj <- st_transform(ncr_sf, crs = 102009)
+
+ncr_excl <- st_buffer(ncr_proj, dist = 200000) # distance of 200 km
+
+ncr_excl_zone <- st_union(ncr_excl)
+ncr_excl_zone <- st_make_valid(ncr_excl_zone)
+
+
+
+plot(ncr_excl_zone)
+
 
 us_count <- ne_countries(country = "united states of america", scale = "medium", returnclass = "sf")
-# continental <- us_count %>%
-#   filter(!name %in% c("Alaska", "Hawaii"))
+us_count <- st_make_valid(us_count)
+
+us_proj <- st_transform(us_count, crs = 102009)
+
+allowed_area <- st_difference(us_proj, ncr_excl_zone)
 
 set.seed(42)
-bg <- st_sample(us_count, size = 100)
+bg <- st_sample(allowed_area, size = 100)
+bg <- st_transform(bg, crs = 4326)
 bg <- as.data.frame(st_coordinates(bg))
+
+allowed_area = st_transform(allowed_area, crs = 4326)
 
 # continental_us <- subset(usa_all, !usa_all$NAME_1 %in% c("Alaska", "Hawaii"))
 
@@ -69,4 +91,4 @@ st_write(sf_map.ncr, 'data/ncr.shp', 'ncr', driver='ESRI Shapefile', append = FA
 plot(ras_ras, main='NCR Presence and Absence')
 points(bg, col='red', pch = 16,cex=.3)
 points(ncr, col='black', pch = 16,cex=.3)
-plot(wrld_simpl, add=TRUE, border='dark grey')
+plot(wrld_smpl, add=TRUE, border='dark grey')
